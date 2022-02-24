fe82e3ba7ba5506ac2788b2e96772b6d980b841b
vpt03 <97585809+Vpt03@users.noreply.github.com>
2022-02-24 10:14:37 +0000

    p3 done

--- p1final.c
+++ p1final.c
@@ -1,1 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+void input(float*x1,float*y1,float*x2 ,float*y2 )
+{
+ printf("enter the numbers\n");
+ scanf("%f%f%f%f",x1,y1,x2,y2);
+  }
+void find_distance(float x1,float y1,float x2, float y2,float *distance)
+{
+  *distance=sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+  
+  
+}
+void output(float distance )
+{
+  printf("distance is %f",distance);
+  
+}
+int main()
+{
+  float x1,y1,x2,y2,distance;
+  input(&x1,&y1,&x2,&y2);
+   find_distance(x1, y1, x2, y2,&distance);
+  output(distance);
+  return 0;
+  }  
+  
 
--- p3final.c
+++ p3final.c
@@ -1,1 +1,36 @@
 
+#include<stdio.h>
+#include<math.h>
+int input()
+{
+  int a;
+  printf("enter a number\n");
+  scanf("%d",&a);
+  return a;
+  }
+int is_prime(int a)
+{
+  if(a==0 || a==1)
+    return 0;
+  for(int i=2;i<=sqrt(a);i++)
+    {
+      if(a%i==0)
+        return 0;
+    }
+  return 1;
+}
+void output(int n,int prime)
+{
+  if(prime==1)
+    printf("the number is prime number\n");
+      else
+    printf("the number is not a prime a number\n");
+}
+int main()
+{
+  int n,result;
+  n=input();
+  result=is_prime(n);
+  output(n,result);
+  return 0;
+}
